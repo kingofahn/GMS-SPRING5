d1ebb9e1aeb00ef7410e212e98e6427229d82a81
kingofahn <kingofahn@gmail.com>
2018-09-13 19:19:18 +0900

    slightly changed

--- src/main/webapp/resources/js/algo.js
+++ src/main/webapp/resources/js/algo.js
@@ -1,40 +1,93 @@
-"use strict";
+"use strict"; 
 var algo = algo || {};
 algo = {
-        init : x =>{
-            /*alert('step 1' + x);*/
-            algo.onCreate(x);
-            algo.setContentView();
-        },
-        onCreate : x=>{
-            algo.router.onCreate(x);
-        },
-        setContentView : ()=>{
-            $('#wrapper').empty();
-        }
+    init :x=>{
+    	algo.onCreate(x);
+    	algo.setContentView();
+    },
+    onCreate:x=>{
+    	algo.router.onCreate(x);
+    },
+    setContentView:()=>{
+        $('#wrapper').empty();
+    }
 };
-
 algo.main = {
-        onCreate : () =>{
-            /*alert('step 4 ::' + $.ctx());*/
-            algo.main.setContentView();
-        },
-        setContentView : ()=>{
-            /*alert('step 5 ::' + $.ctx());*/
-            $('#wrapper').html('<h1>알고리즘</h1><h3>수열</h3><div id="ctn">'
-            		+'<table id="tbl" style"width:800px;height:300px;'
-            		+'border-collapse: collapse; border:1px solid black; margin:0 auto">'
-            		+'<tr style="border: 1px solid black;">'
-            		+'<td id="t__1" style="width: 50%;border:1px solid black;"></td>'
-            		+'<td id="t__r" style="width: 50%;border:1px solid black;"></td>'
-            		+'</tr>'
-            		+'<tr style="border: 1px solid black;">'
-            		+'<td style="width: 50%;border:1px solid black;"> <h3>결과값</h3></td>'
-            		+'<td id="t__a" style="width: 50%;border:1px solid black;"></td>'
-            		+'</tr>'
-            		+'</table>'
-        			+'</div>');
-            $('#t__1').html('<a id="arith__seq"><h3>등차수열</h3></a>');
+    onCreate:()=>{
+    	algo.main.setContentView();
+    },
+    setContentView : ()=>{
+        $('#wrapper').html('<h1>알고리즘</h1><h3>수 열</h3><div id="ctn">'
+    		+'<table id="tbl" style="width:800px;height:300px;'
+    		+'border-collapse: collapse;border: 1px solid black;margin:0 auto">'
+    		+ '<tr style="border: 1px solid black;">'
+    		+  '<td id="t__l" style="width: 50%;border: 1px solid black;"></td>'
+    		+  '<td id="t__r" style="width: 50%;border: 1px solid black;"></td>'
+    		+ '</tr>'
+    		+'</table>'
+    		+'</div>');
+        let $t__l = $('#t__l');
+        let $t__r = $('#t__r');
+        $("<ul />")
+        .attr({id : 'side__menu'})
+        .addClass('list-group').appendTo($t__l);
+       $('<li/>')
+       .attr({id : 'arith'})
+       .addClass('list-group-item')
+       .appendTo($('#side__menu'));
+       $('<a/>')
+       .attr({href : '#'})
+       .html('등차수열의 합')
+       .appendTo($('#arith'))
+       .click(e=>{
+    	$t__r.empty();
+
+    	$('<div/>').attr({id:'ques'}).appendTo($t__r);
+       	$('<h4/>').html('시작값 x, 마지막값 y, 공차 d 인 등차수열의 합을 구하시오.').appendTo($('#ques'));
+       	$('<label/>').html('시작값 : ').appendTo($('#ques'));
+       	$('<input/>').attr({id:'sta',type:'text'}).appendTo($('#ques'));
+       	$('<br/>').appendTo($('#ques'));
+       	$('<label/>').html('마지막값 : ').appendTo($('#ques'));
+       	$('<input/>').attr({id:'end',type:'text'}).appendTo($('#ques'));
+       	$('<br/>').appendTo($('#ques'));
+       	$('<label/>').html('공차 : ').appendTo($('#ques'));
+       	$('<input/>').attr({id:'diff',type:'text'}).appendTo($('#ques'));
+       	$('<br/>').appendTo($('#ques'));
+       	
+       	$('<button/>')
+       	.addClass('btn btn-primary')
+       	.attr({type:'button'})
+       	.html('결과보기')
+       	.appendTo($('#ques'))
+       	.click(e=>{
+       		let a = $('<h6/>').attr({id:'checker'});
+       		$('#checker').remove();
+       		if(($.fn.zeroChecker(
+        	        [$('#sta').val(),
+            	        $('#end').val(),
+            	        $('#diff').val()]))){
+       			a.attr({id:'checker'}).html('빈칸을 채우세요').appendTo($('#ques'));
+       		}else{
+            	let sta = $('#sta').val()*1;
+            	let end = $('#end').val()*1;
+            	let diff = $('#diff').val()*1;
+            	let sum = 0;
+            	let i =0;
+            	while(i<end){
+            		sum+=sta;
+            		sta=sta+diff;
+            		i+=diff;
+            		console.log(sum);
+            	}
+            	a.html('결과값 : '+sum).appendTo($('#ques'));
+       		}
+       	});
+    });
+       
+    }
+};        
+            
+/*         $('#t__1').html('<a id="arith__seq"><h3>등차수열</h3></a>');
             $('#t__1').append('<a id="swit__seq"><h3>스위치수열</h3></a>')
             $('#t__1').append('<a id="fibo__seq"><h3> 피보나치수열</h3></a>')
             $('#t__1').append('<a id="fact__seq"><h3>팩토리얼 함수</h3></a>')
@@ -46,8 +99,16 @@ algo.main = {
             		+'	<label for="마지막값">마지막값</label>	<input id="e" type="text" value="">'
             		+'	<label for="공차">공차</label>	<input id="d" type="text" value="">'
             		+'	<button id="bt"> 결과보기</button>'
+            		+'	<h6 id="rs"> </h6>'
             	$('#t__r').html(ques);
             	$('#bt').click(()=>{
+        			$('#rs').empty().text(($.fn.zeroChecker([
+        				$('#s').val(),
+        				$('#e').val(),
+        				$('#D').val()]))?
+        					'빈칸을 채우세요'
+        					:
+        					'입력값이 모두 정상으로 입력되었습니다.');
                 	let sta = $('#s').val()*1;
                 	let end = $('#e').val()*1;
                 	let diff = $('#d').val()*1;
@@ -68,10 +129,18 @@ algo.main = {
             		'	<h3>시작값 x와 마지막값 y의 +/1 스위치 합을 구하시오(1-2+3-4...)</h3>'
             		+'	<label for="시작값">시작값</label>	<input id="s" type="text" value="">'
             		+'	<label for="마지막값">마지막값</label>	<input id="e" type="text" value="">'
-            		/*+'	<label for="공차">공차</label>	<input id="d" type="text" value="">'*/
+            		+'	<label for="공차">공차</label>	<input id="d" type="text" value="">'
             		+'	<button id="bt"> 결과보기</button>'
+            		+'	<h6 id="rs"> </h6>'
             	$('#t__r').html(ques);
             	$('#bt').click(()=>{
+            		$('#rs').empty().text(($.fn.zeroChecker([
+        				$('#s').val(),
+        				$('#e').val(),
+        				]))?
+        					'빈칸을 채우세요'
+        					:
+        					'입력값이 모두 정상으로 입력되었습니다.');
                 	let sta = $('#s').val()*1;
                 	console.log('sta :: '+sta);
                 	let end = $('#e').val()*1;
@@ -103,14 +172,17 @@ algo.main = {
             			let a1 = $('#s').val()*1;
                     	let a2 = $('#e').val()*1;
                     	let n = $('#n').val()*1;
-                    	
+                    	alert($.fn.zeroChecker([
+            				$('#s').val(),
+            				$('#e').val(),
+            				$('#n').val()]));
             			$('#rs').empty().text(($.fn.zeroChecker([
             				$('#s').val(),
             				$('#e').val(),
             				$('#n').val()]))?
             					'빈칸을 채우세요'
             					:
-            					'완성하세요');
+            					'입력값이 정상으로 입력되었습니다.');
                     	let sum = a1+a2;
                     	let a3 = 0;
                     	let i = 2;
@@ -127,7 +199,7 @@ algo.main = {
     		});
             $('#fact__seq').click(e=>{alert('팩토리얼 함수 선택')});
         }
-};
+};*/
 
 algo.series = {
         arith : x =>{},
